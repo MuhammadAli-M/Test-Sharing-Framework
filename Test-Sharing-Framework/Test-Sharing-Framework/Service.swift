69814f3d743bc5eb3fe3c9d4c5657c6c80f99e78
Doaa Fadally <DoaaFadally@Muhammad-Ali.local>
2018-02-20 16:54:06 +0200

    Adding framework files

---
+++ Test-Sharing-Framework/Test-Sharing-Framework/Service.swift
@@ -0,0 +1,19 @@
+//
+//  Service.swift
+//  TestSharingFramework
+//
+//  Created by Doaa Fadally on 2/20/18.
+//  Copyright © 2018 MuhammadAli. All rights reserved.
+//
+
+import Foundation
+
+class Service{
+    
+    public func addTwoNumbers(num1 first:Int,num2 second:Int) -> Int {
+        return first + second
+    }
+    public init(){
+    }
+}
+
---
+++ Test-Sharing-Framework/Test-Sharing-Framework/Test_Sharing_Framework.h
@@ -0,0 +1,19 @@
+//
+//  Test_Sharing_Framework.h
+//  Test-Sharing-Framework
+//
+//  Created by Doaa Fadally on 2/20/18.
+//  Copyright © 2018 MuhammadAli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Test_Sharing_Framework.
+FOUNDATION_EXPORT double Test_Sharing_FrameworkVersionNumber;
+
+//! Project version string for Test_Sharing_Framework.
+FOUNDATION_EXPORT const unsigned char Test_Sharing_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Test_Sharing_Framework/PublicHeader.h>
+
+
